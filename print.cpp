4443727b73f1cda3beadae56da141d3c2c1c794d
Al Amin <111590949+Alaminal03@users.noreply.github.com>
2026-05-21 18:43:03 +0600

    Add files via upload

---
+++ print.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main()
+{
+    int x = 10;
+    char c = 'A';
+    float a = 10.34;
+    long long int d = 21434354;
+
+    std ::cout << x << std :: endl << c << std :: endl << a << std :: endl << d ;
+
+    return 0;
+}
