cae3e878f8b0740b5844ecf1d086258b7449d481
Al Amin <111590949+Alaminal03@users.noreply.github.com>
2026-06-07 01:20:30 +0600

    Add files via upload

---
+++ switch.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int day;
+
+    cin >> day;
+
+    switch(day)
+    {
+        case 1:
+           cout << "saturday\n";
+           break;
+        case 2:
+            cout << "sunday\n";
+            break;
+         case 3:
+           cout << "monday\n";
+           break;
+        case 4:
+            cout << "tuesday\n";
+            break;    
+         case 5:
+           cout << "wednesday\n";
+           break;
+        case 6:
+            cout << "thusday\n";
+            break;
+         case 7:
+             cout << "friday\n";
+             break;
+   }
+}
