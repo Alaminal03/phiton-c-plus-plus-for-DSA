419939d5db23e86eab2e0cb69730e80c61191050
Al Amin <111590949+Alaminal03@users.noreply.github.com>
2026-06-18 09:22:05 +0600

    Add files via upload

---
+++ palindrome_array.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,count=0;
+    cin >> n;
+    int a[n];
+
+    for (int i = 0; i < n;i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n/2;i++)
+    {
+            if(a[i]!=a[n-1-i])
+            {
+                count++;
+                break;
+            }
+        
+    }
+    if(count==0)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+
+        return 0;
+}
