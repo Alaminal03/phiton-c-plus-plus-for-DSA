1802a3792b342f693958ed74e32da7781330a94f
Al Amin <111590949+Alaminal03@users.noreply.github.com>
2026-06-20 00:15:31 +0600

    Add files via upload

---
+++ problem_02.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int *sort_it(int n)
+{
+    int *a = new int [n];
+    for (int i = 0; i < n;i++)
+    {
+        cin >> a[i];
+    }
+    sort(a, a + n,greater<int>());
+    return a;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int *x = sort_it(n);
+
+    for (int i = 0; i < n;i++)
+    {
+        cout << x[i] << " ";
+        // if(i!=n-1)
+        // {
+        //     cout << " ";
+        // }
+    }
+
+    return 0;
+}
