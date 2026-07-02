1e2564b522b63186047525c4071871bbf6e8da16
Al Amin <111590949+Alaminal03@users.noreply.github.com>
2026-07-02 18:41:45 +0600

    Add files via upload

--- problem_04.cpp
+++ problem_04.cpp
@@ -1,35 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Student
+{
+    public:
+        string name;
+        int cls;
+        char s;
+        int id;
+        int math;
+        int eng;
+};
+
 int main()
 {
-    int n,count=0;
+    int n;
     cin >> n;
 
+    Student a[n];
     for (int i = 0; i < n;i++)
     {
-        string number;
-        int sum = 0;
-
-        for (int i = 0; i < 3;i++)
-        {
-            sum += number[i];
-        }
-        int add = 0;
-        for (int j = 3; j < 6;j++)
-        {
-            add += number[j];
-        }
-        if(sum==add)
-        {
-            cout << "YES" << endl;
-        }
-        else
-        {
-            cout << "NO" << endl;
-        }
-        
+        cin >> a[i].name >> a[i].cls >> a[i].s >> a[i].id >> a[i].math >> a[i].eng;
     }
-    
-        return 0;
+     int mx = INT_MIN;
+     int mxx[n];
+
+     for (int i = 0; i < n;i++)
+     {
+         for (int j = i+1; j < n;j++)
+         {
+             int total_1 = a[i].math + a[i].eng;
+             int total_2 = a[j].math + a[j].eng;
+
+             if(total_2 > total_1)
+             {
+                 swap(a[j], a[i]);
+             }
+             else if(total_1 == total_2)
+             {
+                if(a[j].id < a[i].id)
+                {
+                    swap(a[i],a[j]);
+                }
+             }
+         }
+         
+     }
+
+     for (int i = 0; i < n;i++)
+     {
+        cout << a[i].name << " " << a[i].cls <<" " << a[i].s <<" " << a[i].id <<" " << a[i].math << " " << a[i].eng << " " << endl;
+     }
+
+         return 0;
 }
